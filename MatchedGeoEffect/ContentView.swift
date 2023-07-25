e170e628300b2cf7b6fc0f9a71065279cbba4ae0
ashon96 <andrew.shon@willowtreeapps.com>
2023-07-25 10:05:29 -0400

    Implement trying to use matched geo effect with nav stack

--- MatchedGeoEffect/ContentView.swift
+++ MatchedGeoEffect/ContentView.swift
@@ -8,15 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Namespace private var animation
+    @State private var navPath = [String]()
+
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack(path: $navPath) {
+            VStack {
+                Button {
+                    withAnimation {
+                        navPath.append("HI")
+                    }
+                } label: {
+                    Text("HI")
+                        .matchedGeometryEffect(id: "Hi", in: animation)
+                }
+
+                Button {
+                    withAnimation {
+                        navPath.append("BYE")
+                    }
+                } label: {
+                    Text("BYE")
+                        .matchedGeometryEffect(id: "BYE", in: animation)
+                }
+            }
+            .navigationDestination(for: String.self) { item in
+                VStack {
+                    Spacer()
+                    Text(item)
+                        .matchedGeometryEffect(id: item, in: animation)
+                }
+            }
         }
-        .padding()
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Basic Implementation of matched geometry effect when views
+// are in the same hierachy
+//    @Namespace private var animation
+//    @State private var stateOne = true
+//
+//    var body: some View {
+//
+//        HStack {
+//            if stateOne {
+//                Group {
+//                    Text("Hi")
+//                    Spacer()
+//                }
+//                .matchedGeometryEffect(id: "Test", in: animation)
+//            } else {
+//                Group {
+//                    Spacer()
+//                    Text("HI")
+//                }
+//                .matchedGeometryEffect(id: "Test", in: animation)
+//            }
+//        }
+//        Button("Toggle State") {
+//            withAnimation {
+//                stateOne.toggle()
+//            }
+//        }
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
