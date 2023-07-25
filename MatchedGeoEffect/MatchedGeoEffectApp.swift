15cd04d89a665c4b2f08a122032ea78bf3ca07f4
ashon96 <andrew.shon@willowtreeapps.com>
2023-07-25 09:48:31 -0400

    Initial Commit

---
+++ MatchedGeoEffect/MatchedGeoEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeoEffectApp.swift
+//  MatchedGeoEffect
+//
+//  Created by Andrew Shon on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeoEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
